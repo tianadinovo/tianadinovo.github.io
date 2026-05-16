3e9c5d956c288b88e6dade980f9851d5a32d6667
tianadinovo <tiana.dinovo@protonmail.com>
2026-05-16 18:09:04 -0400

    2026 initial update

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,36 @@
 //data for each image
 const data = [
     {
+        image: 'assets/media/thumb/thumbcamofawn.jfif',
+        full: 'assets/media/camofawn.png',
+        type: 'trad',
+        gallery: 'illus',
+        desc: 'Painting with the fawn sculpture in front of it'
+    },{
+        image: 'assets/media/thumb/thumbpersp.jfif',
+        full: 'assets/media/persp.jfif',
+        type: 'trad',
+        gallery: 'illus',
+        desc: 'Perspective timed study of figures'
+    },{
+        image: 'assets/media/thumb/thumbgoat.jfif',
+        full: 'assets/media/goat.jfif',
+        type: 'trad',
+        gallery: 'illus',
+        desc: 'Pen study of a goat'
+    },{
+        image: 'assets/media/thumb/thumb15model.jfif',
+        full: 'assets/media/15model.jfif',
+        type: 'trad',
+        gallery: 'illus',
+        desc: '15 minute pen study of a model'
+    },{
+        image: 'assets/media/thumb/thumb15model2.jfif',
+        full: 'assets/media/15model2.jfif',
+        type: 'trad',
+        gallery: 'illus',
+        desc: '15 minute pen study of a model'
+    },{
         image: 'assets/media/thumb/thumbcave.jpeg',
         full: 'assets/media/cave.jpeg',
         type: 'digital',
@@ -25,12 +55,6 @@ const data = [
         gallery: 'illus',
         desc: 'Pool'
     },{
-        image: 'assets/media/thumb/thumbmorn.jpeg',
-        full: 'assets/media/morn.png',
-        type: 'trad',
-        gallery: 'illus',
-        desc: 'Winter Morning'
-    },{
         image: 'assets/media/thumb/thumbday.jpeg',
         full: 'assets/media/day.png',
         type: 'trad',
@@ -43,23 +67,11 @@ const data = [
         gallery: 'illus',
         desc: 'Winter Night'
     },{
-        image: 'assets/media/thumb/thumbrain.jpeg',
-        full: 'assets/media/rain.png',
-        type: 'trad',
-        gallery: 'illus',
-        desc: 'Rainy Backyard'
-    }, {
         image: 'assets/media/thumb/thumblove.jpeg',
         full: 'assets/media/love.jpeg',
         type: 'digital',
         gallery: 'illus',
         desc: 'Love of Art - OSA 145th anniv piece'
-    }, {
-        image: 'assets/media/thumb/thumbhorses.jpeg',
-        full: 'assets/media/horses.png',
-        type: 'digital',
-        gallery: 'illus',
-        desc: 'Horse Studies'
     },{
         image: 'assets/media/thumb/thumbodin.jpeg',
         full: 'assets/media/odin.jpeg',
@@ -67,35 +79,23 @@ const data = [
         gallery: 'illus',
         desc: 'Odin'
     },{
-        image: 'assets/media/thumb/thumbroom.jpeg',
-        full: 'assets/media/room.png',
-        type: 'digital',
-        gallery: 'illus',
-        desc: 'Made-up Bedroom'
-    },{
-        image: 'assets/media/thumb/thumbshock.jpeg',
-        full: 'assets/media/shock.png',
-        type: 'digital',
-        gallery: 'illus',
-        desc: 'Shock'
-    },{
-        image: 'assets/media/thumb/thumbowl.jpeg',
-        full: 'assets/media/owl.png',
-        type: 'trad',
-        gallery: 'illus',
-        desc: 'Owl'
-    },{
         image: 'assets/media/thumb/thumbladdy.jpeg',
         full: 'assets/media/laddy.png',
         type: 'digital',
         gallery: 'illus',
         desc: 'Laddy the Lamb'
     },{
-        image: 'assets/media/thumb/thumbarmour.jpeg',
-        full: 'assets/media/armour.gif',
+        image: 'assets/media/thumb/thumbHandler.jfif',
+        full: 'assets/media/Handler.gif',
         type: 'digital',
         gallery: 'three',
-        desc: 'Armour'
+        desc: 'The Handler'
+    },{
+        image: 'assets/media/thumb/thumbFawn.jfif',
+        full: 'assets/media/Fawn.png',
+        type: 'trad',
+        gallery: 'three',
+        desc: 'Crocheted wire Fawn sculpture separate from Camoflage painting'
     },{
         image: 'assets/media/thumb/thumbtank.jpeg',
         full: 'assets/media/tank.gif',
@@ -103,39 +103,65 @@ const data = [
         gallery: 'three',
         desc: 'Tank'
     },{
-        image: 'assets/media/thumb/thumbpaul.jpeg',
-        full: 'assets/media/paul.jpeg',
-        type: 'digital',
-        gallery: 'three',
-        desc: 'Odd Paul'
+        image: 'assets/media/thumb/thumbDogAnim.jfif',
+        full: 'assets/media/DogAnim.gif',
+        type: '0',
+        gallery: 'anim',
+        desc: 'Animation of a dog diving'
     },{
-        image: 'assets/media/thumb/thumbwolf.jpeg',
-        full: 'assets/media/wolf.gif',
-        type: 'digital',
-        gallery: 'three',
-        desc: 'Wolf'
+        image: 'assets/media/thumb/thumbApeAnim.jfif',
+        full: 'assets/media/ApeAnim.gif',
+        type: '0',
+        gallery: 'anim',
+        desc: 'Animation of an ape swinging'
     },{
-        image: 'assets/media/thumb/thumbangel.jpeg',
-        full: 'assets/media/angel.jpeg',
-        type: 'trad',
-        gallery: 'three',
-        desc: 'Angel'
+        image: 'assets/media/thumb/thumbHandlerWalk.jfif',
+        full: 'assets/media/HandlerWalk.gif',
+        type: '0',
+        gallery: 'anim',
+        desc: 'Animated cycle of my Sheridan capstone creature, Handler, walking'
     },{
-        image: 'assets/media/thumb/thumbodinsculpt.jpeg',
-        full: 'assets/media/odinsculpt.jpeg',
-        type: 'trad',
-        gallery: 'three',
-        desc: 'Odin Sculpt'
+        image: 'assets/media/thumb/thumbSluggyPerform.jfif',
+        full: 'assets/media/SluggyPerform.gif',
+        type: '0',
+        gallery: 'anim',
+        desc: 'Animation of an aquatic creature swimming'
     },{
-        image: 'assets/media/thumb/thumbgarg.jpeg',
-        full: 'assets/media/garg.jpeg',
-        type: 'digital',
-        gallery: 'three',
-        desc: 'Gargoyle'
+        image: 'assets/media/thumb/thumbSluggySwim.jfif',
+        full: 'assets/media/SluggySwim.gif',
+        type: '0',
+        gallery: 'anim',
+        desc: 'Animation of an aquatic creature swimming'
+    },{
+        image: 'assets/media/thumb/thumbDogSwim.jfif',
+        full: 'assets/media/DogSwim.gif',
+        type: '0',
+        gallery: 'anim',
+        desc: 'Animated cycle of a dog swimming'
+    },{
+        image: 'assets/media/thumb/thumbDogRun.jfif',
+        full: 'assets/media/DogRun.gif',
+        type: '0',
+        gallery: 'anim',
+        desc: 'Animated cycle of a dog running'
+    },{
+        image: 'assets/media/thumb/thumbDogWalk.jfif',
+        full: 'assets/media/DogWalk.gif',
+        type: '0',
+        gallery: 'anim',
+        desc: 'Animated cycle  of a dog walking'
+    },{
+        image: 'assets/media/thumb/thumbGooseFlight.jfif',
+        full: 'assets/media/GooseFlight.gif',
+        type: '0',
+        gallery: 'anim',
+        desc: '2D animation of a goose in flight'
     }
 ]
 
+
 //defining galleries
+const anim = document.querySelector("#animGallery");
 const threeD = document.querySelector("#modellingGallery");
 const illus = document.querySelector("#illusGallery");
 
@@ -149,9 +175,10 @@ const modRadio = document.querySelector('#modFilter')
 const galleryOneRadio = document.getElementsByName('galleryOne');
 const galleryTwoRadio = document.getElementsByName('galleryTwo');
 
-
-function galleryLoad(galleryOneFilter = '', galleryTwoFilter = ''){
+//anim is gallery 3 as it is the newest addition
+function galleryLoad(galleryOneFilter = '', galleryTwoFilter = '', galleryThreeFilter = ''){
     //clear galleries every time button is clicked, so it doesnt duplicate
+    anim.innerHTML = '';
     threeD.innerHTML = '';
     illus.innerHTML = '';
     
@@ -162,14 +189,16 @@ function galleryLoad(galleryOneFilter = '', galleryTwoFilter = ''){
         if (d.gallery === 'three' && d.type.includes(galleryTwoFilter)) {
             return d;
         }
+        if (d.gallery === 'anim' && d.type.includes(galleryThreeFilter)) {
+            return d;
+        }
     }).forEach((d) => {
         //create div for gallery items
         const newEl = document.createElement('div');
         //add the class for gallery items
         newEl.classList.add('galleryItem');
         //inside the div will be
-        //<a target="_blank" href="${d.full}"> + the closing </a>
-        newEl.innerHTML = `<img src="${d.image}" alt="${d.desc}"><div class="galleryDesc">${d.desc}</div>`;
+        newEl.innerHTML = `<img src="${d.image}">`;
         //check gallery in data(d) for illus or three
         switch(d.gallery){
             case 'illus':
@@ -177,6 +206,9 @@ function galleryLoad(galleryOneFilter = '', galleryTwoFilter = ''){
             break;
             case 'three':
                 threeD.appendChild(newEl);
+            break;
+            case 'anim':
+                anim.appendChild(newEl);
             break;
         };
         newEl.addEventListener('click',(e) => {
@@ -196,7 +228,7 @@ function galleryLoad(galleryOneFilter = '', galleryTwoFilter = ''){
              } else {
                  newEl.classList.remove('enlargedGalleryItem');
                  newEl.classList.add('galleryItem');
-                 newEl.innerHTML =`<img src="${d.image}" alt="${d.desc}"><div class="galleryDesc">${d.desc}</div>`;
+                 newEl.innerHTML =`<img src="${d.image}" alt="${d.desc}">`;
              }
             
          })
@@ -227,3 +259,4 @@ function galleryFilterSelection() {
     //call gallery load function above
     galleryLoad(checkedOneType, checkedTwoType);
 }
+
